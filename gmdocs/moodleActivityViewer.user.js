da7a29f8602a14110ae61ac580e41e9c2c84a32b
Damien Clark <damo.clarky@gmail.com>
2013-08-10 14:36:37 +1000

    Fixed bug with vertical positioning of the configuration jquery dialog by wrapping the settingsDialogDiv.html into another div element structure.
    Also updated the moodleActivityViewer.user.js file with a new version number, which was omitted from last commit.

--- gmdocs/moodleActivityViewer.user.js
+++ gmdocs/moodleActivityViewer.user.js
@@ -2,7 +2,7 @@
 // @name          Moodle Activity Viewer
 // @namespace	    http://damosworld.wordpress.com
 // @description	  Re-render Moodle pages to show student usage
-// @version       0.3.2
+// @version       0.3.3
 // @grant         GM_getValue
 // @grant         GM_setValue
 // @grant         GM_getResourceText
